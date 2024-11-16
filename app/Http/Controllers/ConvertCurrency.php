9c6dab68e49ff26250f8a37e63324f17322adce8
nikolastojkov <stojkov@protonmail.ch>
2024-11-16 15:13:59 +0100

    feat(CurrencyConversion): Request class
    Moved validation to a separate request class, just a bit cleaner and might help in future cases

--- app/Http/Controllers/ConvertCurrency.php
+++ app/Http/Controllers/ConvertCurrency.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CurrencyConversionRequest;
 use App\Services\CurrencyConversionService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 final class ConvertCurrency extends Controller
 {
@@ -12,14 +12,9 @@ final class ConvertCurrency extends Controller
     {
     }
 
-    public function __invoke(Request $request): JsonResponse
+    public function __invoke(CurrencyConversionRequest $request): JsonResponse
     {
-        $validated = $request->validate(rules: [
-            'source_currency' => 'required|string|size:3',
-            'target_currency' => 'required|string|size:3',
-            'value' => 'required|numeric|min:0.01',
-            'state' => 'string'
-        ]);
+        $validated = $request->validated();
 
         $responseObject = $this->service->convertCurrency(
             sourceCurrency: strtoupper(string: $validated['source_currency']),
